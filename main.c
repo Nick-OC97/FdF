cbe7b457cd01435d64fc4288d7b240a5bf31c905
Nicholas O'CONNELL <no-conne@c4r22s1.wethinkcode.co.za>
2019-07-04 12:48:22 +0200

    using some putpixels

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: no-conne <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: no-conne <no-conne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 09:07:25 by no-conne          #+#    #+#             */
-/*   Updated: 2019/07/04 09:42:01 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/07/04 12:46:30 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,22 @@ int	main(void)
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
+	int x;
+	int y;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 200, 200, "NICK");
+	y = 50;
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "NICK");
+	while (y <= 450)
+	{
+		x = 50;
+		while (x <= 450)
+		{
+			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF);
+			x++;
+		}
+	y++;
+	}
 	mlx_loop(mlx_ptr);
 	return (0);
 }
