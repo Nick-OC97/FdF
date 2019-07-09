81ade60c190b1b19765869d32011d5e852c749de
Nicholas O'CONNELL <no-conne@c4r22s1.wethinkcode.co.za>
2019-07-09 13:38:57 +0200

    can now take input file and create array of points

--- main.c
+++ main.c
@@ -6,49 +6,83 @@
 /*   By: no-conne <no-conne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 09:07:25 by no-conne          #+#    #+#             */
-/*   Updated: 2019/07/05 10:17:20 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/07/09 13:37:46 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "/goinfre/no-conne/Desktop/fdfh/resources/minilibx_macos/mlx.h"
-//#include "/goinfre/no-conne/Desktop/fdfh/libft/includes/libft.h"
 #include "main.h"
 
-t_point		get_point(char *str) //turns numbers into points that can be worked with
+t_grid_sizes		*ft_lstnew_fdf(int x_size, int y_size, int total_size)// puts column, row and total size in a struct
 {
-	t_point point;
-	point.x = str[0];
-	point.y = str[1];
-	point.z = str[2];
-	return (point);
+	t_grid_sizes	*grid;
+	if (!(grid = (t_grid_sizes *)malloc(sizeof(t_grid_sizes))))
+		return (NULL);
+	grid->num_x = x_size;
+	grid->num_y = y_size;
+	grid->num_tot = total_size;
+	return(grid);
 }
 
-char **interpreter(const char* path) //turns input file into a str of numbers
+t_grid_sizes		*get_sizes(void) //counts column, row and total size 
+{
+	int x_size;
+	int total_size;
+	int y_size;
+	int fd;
+	char *line;
+	char **inf;
+	t_grid_sizes *grid;
+
+	x_size = 0;
+	fd = open("./test_maps/42.fdf", O_RDONLY);
+	while(get_next_line(fd, &line) > 0)
+	{
+		x_size = 0;
+		inf = ft_strsplit(line, ' ');
+		while (inf[x_size])
+		{
+			total_size++;
+			x_size++;
+		}
+		y_size++;
+	}
+
+	grid = ft_lstnew_fdf(x_size, y_size, total_size);
+	return (grid);
+}
+
+t_point *interpreter(const char* path, t_grid_sizes *grid) //turns string of characters into arry of points
 {
 	int fd;
 	char *line;
 	char **inf;
 	int x;
 	int y;
-	char data[50][50];
+	t_point *data_s;
+	t_point *data;
 
-	x = 0;
 	y = 0;
+	data = (t_point *)malloc(sizeof(t_point) * grid->num_tot);
+	data_s = data;
 	fd = open(path, O_RDONLY);
-	while (get_next_line(fd, &line) > 0);
+	while (get_next_line(fd, &line) > 0)
 	{
+		x = 0;
 		inf = ft_strsplit(line, ' ');
-		while (inf[x][y])
+		while (x < grid->num_x)
 		{
-			data[x][y] = ft_atoi(inf[x][y]);
+			data->x = x;
+			data->y = y;
+			data-> z= ft_atoi(inf[x]);
 			x++;
+			data++;
 		}
 		y++;
 	}
-	return (data);
+	return (data_s);
 }
 
-int	my_key_funct(int keycode, void *param) // for escaping to end function
+int	my_key_funct(int keycode, void *param) //stopping loop with esc function
 {
 	ft_putstr("Key event: ");
 	ft_putnbr(keycode);
@@ -64,33 +98,15 @@ int	main(void)
 	int x;
 	int y;
 	t_mlx_acc	acc;
+	t_point		*lst;
+	t_grid_sizes	*grid;
 
 	mlx_ptr = mlx_init();
-	y = 50;
 	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "NICK");
 	acc.mlx_ptr = mlx_ptr;
 	acc.win_ptr = win_ptr;
-	while (y <= 450)
-	{
-		x = 50;
-		while (x <= 450)
-		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF);
-			x++;
-		}
-	y += 10;
-	}
-	x = 50;
-	while(x <= 450)
-	{
-		y = 50;
-		while (y <= 450)
-		{
-			mlx_pixel_put(mlx_ptr, win_ptr, x, y, 0xFFFFFF);
-			y++;
-		}
-	x += 10;
-	}
+	grid = get_sizes();
+	lst = interpreter("./test_maps/42.fdf", grid);
 	mlx_key_hook(win_ptr, my_key_funct, &acc);
 	mlx_loop(mlx_ptr);
 	return (0);
--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 /*   By: no-conne <no-conne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/04 14:07:49 by no-conne          #+#    #+#             */
-/*   Updated: 2019/07/05 09:08:27 by no-conne         ###   ########.fr       */
+/*   Updated: 2019/07/09 11:26:44 by no-conne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,12 @@
 
 # include "libft/includes/libft.h"
 # include "resources/minilibx_macos/mlx.h"
+
+/* typedef struct s_point_list
+{
+	t_point		point;
+	struct s_point_list *next;
+}				t_point_list;*/
 
 typedef struct s_mlx_acc
 {
@@ -27,6 +33,12 @@ typedef struct s_point
 	int		x;
 	int		y;
 	int		z;
-}				t_point;
+}			t_point;
 
+typedef struct s_grid_sizes
+{
+	int		num_x;
+	int		num_y;
+	int		num_tot;
+}				t_grid_sizes;
 #endif
